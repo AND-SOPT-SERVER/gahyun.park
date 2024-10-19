9c43e1dd2d765099cec033ff60d76722c76208d7
gahyuun <pgh123166@naver.com>
2024-10-19 21:54:20 +0900

    chore: Error Response class -> record

--- src/main/java/org/sopt/diary/api/ErrorResponse.java
+++ src/main/java/org/sopt/diary/api/ErrorResponse.java
@@ -1,13 +1,4 @@
 package org.sopt.diary.api;
 
-public class ErrorResponse implements Response {
-    private final String message;
-
-    public ErrorResponse(String message) {
-        this.message = message;
-    }
-
-    public String getMessage() {
-        return message;
-    }
+public record ErrorResponse(String message) implements Response {
 }
