a72fa889de9e74c3d29fabd753338c20ddbb6a83
gahyuun <pgh123166@naver.com>
2024-10-30 16:57:37 +0900

    feat: user controller 생성

---
+++ src/main/java/org/sopt/diary/api/UserController.java
@@ -0,0 +1,33 @@
+package org.sopt.diary.api;
+
+import org.sopt.diary.dto.ErrorResponse;
+import org.sopt.diary.dto.SignInRequest;
+import org.sopt.diary.dto.SignInResponse;
+import org.sopt.diary.dto.SignUpRequest;
+import org.sopt.diary.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    private final UserService userService;
+
+    public UserController(final UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/users/signup")
+    ResponseEntity<Void> signup(@RequestBody SignUpRequest signUpRequest) {
+        userService.signUp(signUpRequest.loginId(), signUpRequest.password(), signUpRequest.nickname());
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+
+    @PostMapping("/users/signin")
+    ResponseEntity<SignInResponse> signIn(@RequestBody SignInRequest signInRequest) {
+        Long userId = userService.signIn(signInRequest.loginId(), signInRequest.password());
+        return ResponseEntity.status(HttpStatus.OK).body(new SignInResponse(userId));
+    }
+}
